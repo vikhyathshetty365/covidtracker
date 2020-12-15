ff8285edb7a1a3be09fee8ac3831966747a48229
vikhyath shetty <vikhyathshetty365@gmail.com>
2020-12-15 19:41:30 +0530

    charts-mod

--- src/Charts/Charts.jsx
+++ src/Charts/Charts.jsx
@@ -1,107 +1,105 @@
 
 
-import './Charts.css' 
-import {countrystats} from '../api'
+import './Charts.css'
+import { countrystats } from '../api'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import {Bar,Line} from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 import './Charts.css'
-import {getdailydata} from '../api'
+import { getdailydata } from '../api'
 
-const Charts=(props)=>
-
-{
-  const country=props.Country
-  const {data:{confirmed,recovered,deaths}}=props
+const Charts = (props) => {
+  const country = props.Country
+  const { data: { confirmed, recovered, deaths } } = props
 
   console.log(props)
-  
- 
-  const[dailydata,setdailydata]=useState([])
-  
-useEffect(()=>{
-
-const fetchdailydata=async ()=>{
-const dailydata=await getdailydata()
-setdailydata(dailydata)
-console.log(dailydata)
 
 
-}
+  const [dailydata, setdailydata] = useState([])
 
-fetchdailydata()
+  useEffect(() => {
+
+    const fetchdailydata = async () => {
+      const dailydata = await getdailydata()
+      setdailydata(dailydata)
+      console.log(dailydata)
 
 
-},[])
+    }
+
+    fetchdailydata()
+
+
+  }, [])
 
 
 
 
-    if(!confirmed)
+  if (!confirmed)
     return 'Loading...'
-//console.log(props)
+  //console.log(props)
 
-const state = {
-    labels: ['Confirmed','Recovered','Deaths'],
+  const state = {
+    labels: ['Confirmed', 'Recovered', 'Deaths'],
     datasets: [
       {
-        
-        backgroundColor:['rgb(8, 12, 211,0.4)','rgb(14, 241, 63,0.4)',' rgba(243, 10, 10, 0.4)'],
+
+        backgroundColor: ['rgb(8, 12, 211,0.4)', 'rgb(14, 241, 63,0.4)', ' rgba(243, 10, 10, 0.4)'],
         borderColor: 'rgba(0,0,0,1)',
         borderWidth: 1,
-        data: [confirmed.value,recovered.value,deaths.value]
+        data: [confirmed.value, recovered.value, deaths.value]
       }
     ]
   }
 
 
-const line=(
+  const line = (
 
-  dailydata[0] ? (
-    <Line
-      data={{
-        labels: dailydata.map(({ date }) => new Date(date).toLocaleDateString()),
-        datasets: [{
-          data: dailydata.map((data) => data.positive),
-          label: 'Infected',
-          borderColor: '#3333ff',
-          fill: true,
-        }
-        ],
-      }}
-    />
-  ) : null
-
-
-);
+    dailydata[0] ? (
+      <Line
+        data={{
+          labels: dailydata.map(({ date }) => new Date(date).toLocaleDateString()),
+          datasets: [{
+            data: dailydata.map((data) => data.positive),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          }
+          ],
+        }}
+      />
+    ) : null
 
 
-const bar=(
-confirmed?(
-<Bar
+  );
+
+
+  const bar = (
+    confirmed ? (
+      <Bar
         data={state}
-                
-        />
+
+      />
 
 
 
-):null
+    ) : null
 
 
-);
+  );
 
 
 
 
-return(
-    
+  return (
+
     <div className="chartmaindiv">
-{country?bar:line}
+      {country ? bar : null}
 
 
 
 
     </div>
-)
+  )
 }
 export default Charts
--- src/countrypicker/Countrypicker.jsx
+++ src/countrypicker/Countrypicker.jsx
@@ -1,52 +1,51 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Countrypicker.css'
 import { NativeSelect, FormControl } from '@material-ui/core';
 import { countrydata } from '../api';
 
-const Countrypicker=({handlechange})=>{
-    const[country,setcountry]=useState([])
-    
+const Countrypicker = ({ handlechange }) => {
+    const [country, setcountry] = useState([])
 
-    useEffect(()=>{
-    
-    const getcountrydata=async()=>
-    {
-     const countries=await countrydata();   
-     setcountry(countries)
-    
-    }
-    getcountrydata()
-    console.log(country)
-   
-    },[])
 
+    useEffect(() => {
+
+        const getcountrydata = async () => {
+            const countries = await countrydata();
+            setcountry(countries)
+
+        }
+        getcountrydata()
+        console.log(country)
+
+    }, [])
 
 
 
-    return(
 
-<div className="form_div">
+    return (
 
-      
-
-<FormControl >
-<NativeSelect defaultValue="Global"  onChange={(e)=>handlechange(e.target.value)}  >
-<option value="">Global</option>
-{country.map((country,i)=><option key={i} val={country}>{country}</option>)}
-
-</NativeSelect>
+        <div className="form_div">
 
 
-</FormControl>
 
- 
+            <FormControl >
+                <NativeSelect onChange={(e) => handlechange(e.target.value)}  >
 
-     
-      
-  
-   
-</div>
+                    {country.map((country, i) => (<option key={i} val={country}>{country}</option>))}
+
+                </NativeSelect>
+
+
+            </FormControl>
+
+
+
+
+
+
+
+        </div>
 
 
 
